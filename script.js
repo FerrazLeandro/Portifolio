bba416a0fce0aaaf3a6f9a2760a2cbff98396d3c
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2022-10-03 16:45:13 -0300

    Lançamento V 3.0

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+window.onload = function () { 
+    if (localStorage.getItem("tema") === "ligth") {
+        document.body.classList.toggle('ligth')
+    }
+}
+
+
 document.getElementById("btn-tema").onclick = () => {
-    document.body.classList.toggle('dark')
+
+    if (localStorage.getItem("tema") === "ligth") {
+        document.body.classList.remove('ligth') 
+        localStorage.clear();
+     } else {
+        localStorage.setItem("tema", "ligth")
+        document.body.classList.toggle('ligth')
+     }
 }
